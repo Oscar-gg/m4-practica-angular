6ef72d455b87724010f6192ce96595ddc9c5a638
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-04-24 12:40:19 -0600

    added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BotonComponent } from './boton/boton.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+// import { BotonComponent } from './boton/boton.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BotonComponent, FormsModule],
+  imports: [RouterOutlet, FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ngOscar';
-  edad: number = 20;
-  suma() {
-    this.edad++;
-  }
-  resta() {
-    this.edad--;
-  }
+  // title = 'ngOscar';
+  // edad: number = 20;
+  // suma() {
+  //   this.edad++;
+  // }
+  // resta() {
+  //   this.edad--;
+  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
